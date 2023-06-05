ba7e02e28f3001f70c6345ace8d0486927462250
satri <satriayusufrafaly@gmail.com>
2023-06-05 21:40:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/restaurantreview/ApiService.kt
@@ -0,0 +1,23 @@
+package com.dicoding.restaurantreview
+
+import com.example.restaurantreview.PostReviewResponse
+import com.example.restaurantreview.RestaurantResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+
+    @FormUrlEncoded
+    @Headers("Authorization: token 12345")
+    @POST("review")
+
+    fun postReview(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("review") review: String
+    ): Call<PostReviewResponse>
+}
